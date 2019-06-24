94f0186cef80916dae929122a768bc05964d7d93
Yanina <stalinskaya.yana@mail.ru>
2019-06-24 12:24:25 +0300

    checkpoint

--- Angular/src/app/shared/signal-r.service.ts
+++ Angular/src/app/shared/signal-r.service.ts
@@ -17,7 +17,7 @@ export class SignalRService {
 
     public startConnection = () => {
         this.hubConnection = new signalR.HubConnectionBuilder()
-            .withUrl('https://localhost:44355/api/chat',
+            .withUrl('https://localhost:44355/chat',
                 {
                     skipNegotiation: true,
                     transport: HttpTransportType.WebSockets,
